60cb40facdd14685519aabd5cccd8f0cf137efec
gpetrou <gpetrou@actify.com>
2018-07-21 17:04:30 +0100

    Remove WIN8 classes and enums

--- Src/Microsoft.Scripting/Stubs.cs
+++ Src/Microsoft.Scripting/Stubs.cs
@@ -4,49 +4,6 @@
 
 using System.Reflection;
 
-#if WIN8
-
-// When compiled with Dev10 VS CSC reports errors if this is not defined
-// error CS0656: Missing compiler required member 'System.Threading.Thread.get_ManagedThreadId'
-// error CS0656: Missing compiler required member 'System.Threading.Thread.get_CurrentThread'
-namespace System.Threading {
-    internal class Thread {
-        public int ManagedThreadId { get { throw new NotImplementedException(); } }
-        public static Thread CurrentThread { get { throw new NotImplementedException(); } }
-    }
-}
-
-namespace System.IO {
-    [Serializable]
-    public enum FileMode {
-        CreateNew = 1,
-        Create,
-        Open,
-        OpenOrCreate,
-        Truncate,
-        Append
-    }
-
-    [Serializable]
-    public enum FileAccess {
-        Read = 1,
-        Write = 2,
-        ReadWrite = 3
-    }
-
-    [Serializable]
-    public enum FileShare {
-        None = 0,
-        Read = 1,
-        Write = 2,
-        ReadWrite = 3,
-        Delete = 4,
-        Inheritable = 16
-    }
-}
-
-#endif
-
 #if !FEATURE_SERIALIZATION
 
 namespace System {
