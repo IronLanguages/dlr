17ea77a0663f9e4c2b4bfa91459b0dc85eeaa63f
gpetrou <gpetrou@actify.com>
2018-11-09 10:43:58 +0000

    Add missing licenses

--- Src/Microsoft.Dynamic/Actions/MemberRequestKind.cs
+++ Src/Microsoft.Dynamic/Actions/MemberRequestKind.cs
@@ -1,4 +1,7 @@
-﻿
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
 namespace Microsoft.Scripting.Actions {
     /// <summary>
     /// Specifies the action for which the default binder is requesting a member.
