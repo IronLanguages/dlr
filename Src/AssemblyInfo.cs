37cb9e9069867579b0ffe6ead798ad28d5e584e4
Stéphane Lozier <slozier@users.noreply.github.com>
2019-01-02 15:38:35 -0500

    Remove CurrentVersion

--- Src/AssemblyInfo.cs
+++ Src/AssemblyInfo.cs
@@ -2,26 +2,6 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-using System.Reflection;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyProduct("Dynamic Language Runtime")]
-
-#if DEBUG
-[assembly: AssemblyConfiguration("DEBUG")]
-#else
-[assembly: AssemblyConfiguration("RELEASE")]
-#endif
-
-[assembly: AssemblyCompany("DLR Open Source Team")]
-[assembly: AssemblyCopyright("© DLR Contributors.")]
-
 [assembly: AllowPartiallyTrustedCallers]
-
-// Versioning
-[assembly: AssemblyVersion(DynamicLanguageRuntime.CurrentVersion.AssemblyVersion)]
-[assembly: AssemblyFileVersion(DynamicLanguageRuntime.CurrentVersion.AssemblyFileVersion)]
-[assembly: AssemblyInformationalVersion(DynamicLanguageRuntime.CurrentVersion.AssemblyInformationalVersion)]
