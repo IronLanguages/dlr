fb69caf51c3475d536400fd42d0d967a656aea46
slozier <slozier@users.noreply.github.com>
2019-01-02 20:23:50 -0500

    Remove CurrentVersion and BuildInfo templates (#164)
    * Get rid of BuildInfo
    
    * Remove CurrentVersion
    
    * Remove common AssemblyInfo.cs
    
    * Remove BeforeTargets

--- Tests/Metadata/Properties/AssemblyInfo.cs
+++ Tests/Metadata/Properties/AssemblyInfo.cs
@@ -2,25 +2,8 @@
 // The .NET Foundation licenses this file to you under the Apache 2.0 License.
 // See the LICENSE file in the project root for more information.
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Metadata Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("DLR")]
-[assembly: AssemblyCopyright("© Microsoft Corporation.  All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: ComVisible(false)]
 [module: UnverifiableCode]
