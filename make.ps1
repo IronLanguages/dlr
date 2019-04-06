f03718a04cd582cc1b05f9e94b4b523a4f5773b8
gpetrou <gipetrou@gmail.com>
2019-04-06 10:01:49 +0100

    Update Readme

--- make.ps1
+++ make.ps1
@@ -21,7 +21,7 @@ if(!$global:isUnix) {
     if([System.IO.File]::Exists($_VSWHERE)) {
         $_VSINSTPATH = & "$_VSWHERE" -latest -prerelease -requires Microsoft.Component.MSBuild -property installationPath
     } else {
-        Write-Error "Visual Studio 2017 15.9 or later is required"
+        Write-Error "Visual Studio 2019 16.3.0 or later is required"
         Exit 1
     }
 
