3be69f8f8f4d2490278cec3bc87b980bc7180d70
Stéphane Lozier <slozier@users.noreply.github.com>
2022-05-29 15:49:06 -0400

    Bump to .NET Framework 4.5.2

--- make.ps1
+++ make.ps1
@@ -4,7 +4,7 @@ Param(
     [Parameter(Position=1)]
     [String] $target = "release",
     [String] $configuration = "Release",
-    [String[]] $frameworks=@('net45','netcoreapp2.1','netcoreapp3.1','net5.0'),
+    [String[]] $frameworks=@('net452','netcoreapp2.1','netcoreapp3.1','net5.0'),
     [String] $platform = "x64",
     [switch] $runIgnored
 )
@@ -62,7 +62,7 @@ function GenerateRunSettings([String] $folder, [String] $framework, [String] $pl
 
 #   <RunSettings>
 #     <TestRunParameters>
-#       <Parameter name="FRAMEWORK" value="net45" />
+#       <Parameter name="FRAMEWORK" value="net452" />
 #     </TestRunParameters>
 #   </RunSettings>
 
